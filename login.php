3cf0cf2570e464a43a46b402a1eb1003efb92e24
Rafaela Amboni Ghellere <168464894+rawfaela@users.noreply.github.com>
2025-03-17 14:56:35 -0300

    Add files via upload

--- login.php
+++ login.php
@@ -29,7 +29,7 @@ if (isset($_POST["entrar"])){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="estilos.css" type="text/css">
+    <link rel="stylesheet" href="styles.css" type="text/css">
     <title>Login</title>
 </head>
 <body id="bodylogin">
